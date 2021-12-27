40573faaf555d1ac3f0c7019c186b201c6b9b319
jzl1 <34795003+jzl1@users.noreply.github.com>
2021-12-27 19:22:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-/usr/sbin/sshd -D -p 22
+nohup /usr/sbin/sshd -D -p 22 &&
 echo -e "[common]\nserver_addr = ${server_addr}\nserver_port = ${server_port}\ntoken = ${token}\n\n[ssh]\ntype = tcp\nlocal_ip = 127.0.0.1\nlocal_port = 22\nremote_port = ${remote_port}" >>/etc/frp/frpc.ini
 /frpc -c /etc/frp/frpc.ini
