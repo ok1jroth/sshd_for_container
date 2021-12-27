887081478778f296576bf94690060f2dde701596
jzl1 <34795003+jzl1@users.noreply.github.com>
2021-12-27 20:50:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 nohup /usr/sbin/sshd -D -p 22 &
 echo -e "[common]\nserver_addr = ${server_addr}\nserver_port = ${server_port}\ntoken = ${token}\n\n[ssh]\ntype = tcp\nlocal_ip = 127.0.0.1\nlocal_port = 22\nremote_port = ${remote_port}" >>/etc/frp/frpc.ini
-nohup /agent -s $tz_address -p $tz_secret --skip-conn --skip-procs &
 nohup /frpc -c /etc/frp/frpc.ini &&
 tail -f /nohup.out
